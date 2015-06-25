3f5d8b5b82549ee1514cef85f8490ed8ac57dca9
Ryabenkiy Sergiy <3dextend@gmail.com>
2015-06-26 00:58:15 +0300

    Add random values

--- BubleSort/src/BubbleSortApp.java
+++ BubleSort/src/BubbleSortApp.java
@@ -5,10 +5,15 @@
 public class BubbleSortApp {
 
         public static void main(String[] args) {
-            int maxSize = 100; // Размер массива
+            int maxSize = 50; // Размер массива
             ArrayBub arr; // Ссылка на массив
             arr = new ArrayBub(maxSize); // Создание массива
-            arr.insert(77); // Вставка 10 элементов
+            for(int j=0; j<maxSize; j++) // Заполнение массива
+            { // случайными числами
+                long n = (long)( java.lang.Math.random()*(maxSize-1) );
+                arr.insert(n);
+            }
+            /*arr.insert(77); // Вставка 10 элементов
             arr.insert(99);
             arr.insert(44);
             arr.insert(55);
@@ -20,12 +25,12 @@ public class BubbleSortApp {
             arr.insert(-2);
             arr.insert(-7);
             arr.insert(-20);
-            arr.insert(88);
+            arr.insert(88);*/
             arr.display(); // Вывод элементов
             arr.bubbleSort(); // Пузырьковая сортировка элементов
             arr.display(); // Повторный вывод
         } //
-} // Конец класса BubbleSortApp
+} // Конец класса BubbleSortAppg
 
 
 
