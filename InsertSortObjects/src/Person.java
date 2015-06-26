a3d3d0ee88276b3015c38b0aae291549ba65a2b1
Ryabenkiy Sergiy <3dextend@gmail.com>
2015-06-26 13:52:08 +0300

    Add insert sort method for objects

---
+++ InsertSortObjects/src/Person.java
@@ -0,0 +1,26 @@
+/**
+ * SortsAlgorithm
+ * Created by Ryabenkiy Sergey on 26.06.2015 13:47.
+ */
+public class Person {
+    private String lastName;
+    private String firstName;
+    private int age;
+    //-----------------------------------------------------------
+    public Person(String last, String first, int a)
+    { // Конструктор
+        lastName = last;
+        firstName = first;
+        age = a;
+    }
+    //-----------------------------------------------------------
+    public void displayPerson()
+    {
+        System.out.print(" Last name: " + lastName);
+        System.out.print(", First name: " + firstName);
+        System.out.println(", Age: " + age);
+    }
+    //-----------------------------------------------------------
+    public String getLast() // Получение фамилии
+    { return lastName; }
+} // Конец класса Person
