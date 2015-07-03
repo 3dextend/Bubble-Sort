1a26c12a1244d28f86f28a130104d2da4084494b
Ryabenkiy Sergiy <3dextend@gmail.com>
2015-07-04 00:46:33 +0300

    Add TopoSort algorithm for graph

---
+++ TopoGraphSort/src/Vertex.java
@@ -0,0 +1,15 @@
+/**
+ * SortsAlgorithm
+ * Created by Ryabenkiy Sergey on 04.07.2015 0:41.
+ */
+public class Vertex {
+    public char label; // Метка (например, 'A')
+
+    // -------------------------------------------------------------
+    public Vertex(char lab) // Конструктор
+    {
+        label = lab;
+    }
+}
+
+
